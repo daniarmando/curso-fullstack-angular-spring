5bfce7ba3afb304148648eccc2ac24bd3a825b92
daniarmando <daniel.armando@live.com>
2019-08-28 08:59:58 -0300

    Desafio: Criando o cadastro de pessoa

--- src/main/java/com/example/algamoney/api/resource/CategoriaResource.java
+++ src/main/java/com/example/algamoney/api/resource/CategoriaResource.java
@@ -7,14 +7,12 @@ import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -33,8 +31,7 @@ public class CategoriaResource {
 		return categoriaRepository.findAll();
 	}
 	
-	@PostMapping
-	@ResponseStatus(HttpStatus.CREATED)
+	@PostMapping	
 	public ResponseEntity<Categoria> criar(@Valid @RequestBody Categoria categoria, HttpServletResponse response) {
 		Categoria categoriaSalva = categoriaRepository.save(categoria);
 		URI uri= ServletUriComponentsBuilder.fromCurrentRequest().path("/{codigo}")
